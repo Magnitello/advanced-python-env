2615f2d3a3a1882801cfa72e9505c7caf4f53b42
Magnitello <danialrakishev228@gmail.com>
2025-12-15 17:10:17 +0500

    Create Task6.py

---
+++ week-1/Task6.py
@@ -0,0 +1,11 @@
+FirN = int(input("First Number: "))
+SecN = int(input("Second Number: "))
+Oper = input("Operation: ")
+if Oper == "+":
+    print (FirN + SecN)
+elif Oper == "-":
+    print (FirN - SecN)
+elif Oper == "*":
+    print (FirN * SecN)
+elif Oper == "/":
+    print (FirN / SecN)
