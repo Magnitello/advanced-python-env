5f9c8e3d7b16019adfeec7bd0e75beeea3b13bf7
Magnitello <danialrakishev228@gmail.com>
2025-12-15 17:12:23 +0500

    Create Task9.py

---
+++ week-1/Task9.py
@@ -0,0 +1,7 @@
+Ticket = input()
+FirThree = int(Ticket[0] + Ticket[1] + Ticket[2])
+SecThree = int(Ticket[3] + Ticket[4] + Ticket[5])
+if FirThree == SecThree:
+    print ("Yes")
+else:
+    print ("No")
