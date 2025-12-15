905ee1666c6e42a74ef94a503ca7c406f7418b00
Magnitello <danialrakishev228@gmail.com>
2025-12-15 17:20:36 +0500

    Update Task9.py

--- week-1/Task9.py
+++ week-1/Task9.py
@@ -1,6 +1,6 @@
 Ticket = input()
-FirThree = int(Ticket[0] + Ticket[1] + Ticket[2])
-SecThree = int(Ticket[3] + Ticket[4] + Ticket[5])
+FirThree = int(Ticket[0]) + int(Ticket[1]) + int(Ticket[2])
+SecThree = int(Ticket[3]) + int(Ticket[4]) + int(Ticket[5])
 if FirThree == SecThree:
     print ("Yes")
 else:
