74010c531f169ec1c2a3555ccfc2af52d88c2473
Magnitello <danialrakishev228@gmail.com>
2025-12-15 17:08:11 +0500

    Update Task4.py

--- week-1/Task4.py
+++ week-1/Task4.py
@@ -1,5 +1,3 @@
-mult1 = int(input("Enter number multiplied by 5: "))
-add1 = int(input("Add 8 to prev number: "))
-mult2 = int(input("Enter number multiplied by 2: "))
-ans = (mult2 / 2 - 8) / 5
-print (ans)
+N = int(input())
+Sum = (N * (N + 1)) // 2
+print (Sum)
